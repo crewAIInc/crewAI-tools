604e01252dbd9bae889ee0f75d60b47c99bfccca
“Bisan <bisansuleiman@berkeley.edu>
2024-09-07 15:11:35 -0700

    added sec tools to init files

--- crewai_tools/tools/__init__.py
+++ crewai_tools/tools/__init__.py
@@ -50,3 +50,5 @@ from .youtube_channel_search_tool.youtube_channel_search_tool import (
 )
 from .youtube_video_search_tool.youtube_video_search_tool import YoutubeVideoSearchTool
 from .mysql_search_tool.mysql_search_tool import MySQLSearchTool
+from .sec10k_search_tool.sec10k_search_tool import SEC10KTool
+from .sec10q_search_tool.sec10q_search_tool import SEC10QTool
