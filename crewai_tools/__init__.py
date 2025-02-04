c9d8aee475afe341cb0cab3c36a1f16e25907943
Marco Vinciguerra <mvincig11@gmail.com>
2025-02-04 16:49:56 +0100

    feat: add search tool

--- crewai_tools/__init__.py
+++ crewai_tools/__init__.py
@@ -33,6 +33,7 @@ from .tools import (
     RagTool,
     ScrapeElementFromWebsiteTool,
     ScrapegraphScrapeTool,
+    ScrapegraphSearchTool,
     ScrapegraphScrapeToolSchema,
     ScrapeWebsiteTool,
     ScrapflyScrapeWebsiteTool,
