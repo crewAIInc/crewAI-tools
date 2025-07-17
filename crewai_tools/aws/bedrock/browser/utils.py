dcbdad1064d69bce71ca1600fdd75da1e8b20711
Michael Chin <mchin188@yahoo.com>
2025-07-17 03:54:44 -0700

    Fix 'get_current_page' utilities for Browser tools

--- crewai_tools/aws/bedrock/browser/utils.py
+++ crewai_tools/aws/bedrock/browser/utils.py
@@ -1,38 +1,43 @@
-from typing import Any
+from __future__ import annotations
 
-async def aget_current_page(browser: Any) -> Any:
-    """Get the current page from a browser, or create one if none exists (async).
+from typing import TYPE_CHECKING, Any, Union
 
-    Args:
-        browser: The Playwright browser instance
+if TYPE_CHECKING:
+    from playwright.async_api import Browser as AsyncBrowser
+    from playwright.async_api import Page as AsyncPage
+    from playwright.sync_api import Browser as SyncBrowser
+    from playwright.sync_api import Page as SyncPage
 
-    Returns:
-        The current (or newly created) page
+
+async def aget_current_page(browser: Union[AsyncBrowser, Any]) -> AsyncPage:
     """
-    # Get existing pages
-    pages = await browser.pages()
-    
-    # Return existing page or create new one
-    if pages:
-        return pages[0]
-    else:
-        return await browser.new_page()
-
-
-def get_current_page(browser: Any) -> Any:
-    """Get the current page from a browser, or create one if none exists (sync).
-
+    Asynchronously get the current page of the browser.
     Args:
-        browser: The Playwright browser instance
-
+        browser: The browser (AsyncBrowser) to get the current page from.
     Returns:
-        The current (or newly created) page
+        AsyncPage: The current page.
     """
-    # Get existing pages
-    pages = browser.pages
-    
-    # Return existing page or create new one
-    if pages:
-        return pages[0]
-    else:
-        return browser.new_page()
+    if not browser.contexts:
+        context = await browser.new_context()
+        return await context.new_page()
+    context = browser.contexts[0]
+    if not context.pages:
+        return await context.new_page()
+    return context.pages[-1]
+
+
+def get_current_page(browser: Union[SyncBrowser, Any]) -> SyncPage:
+    """
+    Get the current page of the browser.
+    Args:
+        browser: The browser to get the current page from.
+    Returns:
+        SyncPage: The current page.
+    """
+    if not browser.contexts:
+        context = browser.new_context()
+        return context.new_page()
+    context = browser.contexts[0]
+    if not context.pages:
+        return context.new_page()
+    return context.pages[-1]
