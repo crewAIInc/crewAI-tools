a6cb01db57741e88437e7c2692d00c37db333ec0
Tom Mahler <tom@mahler.tech>
2025-08-14 16:17:39 +0300

    added example of simple usage

--- crewai_tools/tools/browser_use_tool/example.py
+++ crewai_tools/tools/browser_use_tool/example.py
@@ -1,25 +1,17 @@
 import asyncio
 
-from browser_use import Browser, BrowserConfig
-from browser_use.browser.context import BrowserContext
-from crewai import Agent, Crew, Task
+from browser_use.llm import ChatOpenAI as BrowserUseChatOpenAI
+
+from crewai import Agent, Crew, CrewOutput, Task
 
 from crewai_tools.tools.browser_use_tool import BrowserUseTool
-from langchain_openai.chat_models import ChatOpenAI
 
 
-def main():
-
-    browser = Browser(config=BrowserConfig(headless=False))
-
-    browser_context = BrowserContext(browser=browser)
-
-    browser_use_tool = BrowserUseTool(
-        llm=ChatOpenAI(model="gpt-4o"),
-        browser=browser,
-        browser_context=browser_context,
-    )
-
+def run_crew(browser_use_tool: BrowserUseTool) -> CrewOutput:
+    """
+    Run a simple crew with the BrowserUseTool.
+    This function is used to demonstrate how to use the BrowserUseTool in a crew.
+    """
     agent = Agent(
         role="Browser Use Agent",
         goal="Use the browser",
@@ -28,7 +20,7 @@ def main():
             "You have a browser that you can interact with using natural language instructions."
         ),
         tools=[browser_use_tool],
-        verbose=True,
+        # verbose=True,
         # llm="gemini/gemini-2.0-flash-exp",
         llm="gpt-4o",
     )
@@ -41,24 +33,22 @@ def main():
     )
 
     crew = Crew(
-        tasks=[task],
         agents=[agent],
+        tasks=[task],
         verbose=True,
     )
 
-    crew_result = crew.kickoff(
+    return crew.kickoff(
         inputs={
             "webpage": "https://www.wikipedia.org/",
             # "webpage": "https://www.nytimes.com/",
         }
     )
 
-    print(crew_result.raw)
-
-    with asyncio.new_event_loop() as loop:
-        loop: asyncio.AbstractEventLoop
-        loop.run_until_complete(browser.close())
-
+async def simple_browser_interaction():
+    browser_use_tool = BrowserUseTool(llm=BrowserUseChatOpenAI(model="gpt-4o"))
+    crew_output = run_crew(browser_use_tool)
+    print(crew_output.raw)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(simple_browser_interaction())
