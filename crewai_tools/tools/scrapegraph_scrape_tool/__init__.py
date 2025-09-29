e96d4267e3ac4f4fb3eeb98c7d7fda23873c8f05
Marco Vinciguerra <mvincig11@gmail.com>
2025-09-29 12:21:20 +0200

    feat: add scrapegraph

---
+++ crewai_tools/tools/scrapegraph_scrape_tool/__init__.py
@@ -0,0 +1,17 @@
+from .scrapegraph_scrape_tool import (
+    ScrapegraphScrapeTool,
+    ScrapegraphScrapeToolSchema,
+    FixedScrapegraphScrapeToolSchema,
+    ScrapeMethod,
+    ScrapegraphError,
+    RateLimitError,
+)
+
+__all__ = [
+    "ScrapegraphScrapeTool",
+    "ScrapegraphScrapeToolSchema",
+    "FixedScrapegraphScrapeToolSchema",
+    "ScrapeMethod",
+    "ScrapegraphError",
+    "RateLimitError",
+]
